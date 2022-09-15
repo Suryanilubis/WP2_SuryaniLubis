21fbc1bc65ee95be8ad1d0f26c9229e5c76819ca
Suryanilubis <113574771+Suryanilubis@users.noreply.github.com>
2022-09-15 16:03:47 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo"Nama saya Suryani Lubis
+            Saya tinggal di daerah tambun selatan 
+            Olahraga yang saya sukai adalah berenang";
+    }
+}
+         
